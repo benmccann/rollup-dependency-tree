de00c7d335b11a9e1933932ab1e33e9378000bc8
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-08-27 13:21:08 -0700

    Make more configurable

--- index.ts
+++ index.ts
@@ -1,23 +1,46 @@
 import type { OutputChunk } from 'rollup';
 
-export default function(chunks: OutputChunk[]): Record<string,string[]> {
+export interface DependencyTreeOptions {
+  includeRoot: boolean;
+}
+
+/**
+ * Returns the transitive dependencies for all chunks
+ * @param chunks - all chunks. dependencies will be calculated for each
+ * @param [opts] - the options to use
+ * @return a map from chunk.facadeModuleId to dependencies
+ */
+export function dependenciesForForest(chunks: OutputChunk[], opts?: DependencyTreeOptions): Record<string,string[]> {
   const result: Record<string,string[]> = {};
   chunks.filter(chunk => chunk.facadeModuleId)
-      .forEach(chunk => { result[chunk.facadeModuleId] = Array.from(getImports(chunks, [chunk])); });
+      .forEach(chunk => { result[chunk.facadeModuleId] = Array.from(dependenciesForTree(chunk, chunks, opts)); });
   return result;
 };
 
-function getImports(allChunks: OutputChunk[], chunksToResolve: OutputChunk[]): Set<string> {
+/**
+ * Returns the transitive dependencies for a certain chunk
+ * @param chunk - the chunk to get the dependencies of
+ * @param chunks - all chunks
+ * @param [opts] - the options to use
+ * @return the transitive dependencies for the given chunk
+ */
+export function dependenciesForTree(chunk: OutputChunk, allChunks: OutputChunk[], opts?: DependencyTreeOptions): Set<string> {
+  return dependenciesForTrees([chunk], allChunks, opts);
+}
+
+function dependenciesForTrees(chunksToResolve: OutputChunk[], allChunks: OutputChunk[], opts?: DependencyTreeOptions): Set<string> {
   const result = new Set<string>();
   chunksToResolve.forEach(chunk => {
     chunk.imports.forEach(fileName => {
       if (!result.has(fileName)) { // avoid cycles
         result.add(fileName);
         let importedChunks = allChunks.filter(chunk => chunk.fileName === fileName);
-        getImports(allChunks, importedChunks).forEach(fileName => result.add(fileName));
+        dependenciesForTrees(importedChunks, allChunks).forEach(fileName => result.add(fileName));
       }
     });
-    result.add(chunk.fileName);
+    if (opts && opts.includeRoot) {
+      result.add(chunk.fileName);
+    }
   });
   return result;
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import type { OutputChunk } from 'rollup';
 
-import dependencyTree from '../index';
+import { dependenciesForForest } from '../index';
 import realworld from './realworld';
 
 describe('test suite', () => {
-  it('dependencyTree should handle sveltejs/realworld example', () => {
+  it('dependenciesForForest should handle sveltejs/realworld example', () => {
     const rollupData: OutputChunk[] = realworld as OutputChunk[];
-    const result = dependencyTree(rollupData);
+    const result = dependenciesForForest(rollupData, { includeRoot: true });
     const deps = result['/home/bmccann/src/svelte-realworld/src/routes/index.svelte'];
     expect(deps.length).equal(5);
   })
